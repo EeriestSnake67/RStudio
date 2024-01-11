e41442e7bb301be67161e4e8ba440503b6dda35c
EeriestSnake67 <dranoel927@gmail.com>
2024-01-11 13:23:57 -0500

    Initial Commit

---
+++ Candlestick.R
@@ -0,0 +1,10 @@
+# Load the necessary library
+library(readr)
+
+# Read the CSV file
+BAC <- read_csv("Data/BAC.csv")
+C <- read_csv('Data/C.csv')
+GS <- read_csv('Data/GS.csv')
+JPM <- read_csv('Data/JPM.csv')
+MS <- read_csv('Data/MS.csv')
+WFC <- read_csv('Data/WFC.csv')
