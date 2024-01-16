5e2da9cc13b50c22b97b324e4e07776a0b227e92
EeriestSnake67 <dranoel927@gmail.com>
2024-01-16 13:19:20 -0500

    BAC Candlestick graph

--- Candlestick.R
+++ Candlestick.R
@@ -3,6 +3,7 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(GGally)
+library(plotly)
 
 # Read the CSV file
 BAC <- read_csv("Data/BAC.csv")
@@ -21,16 +22,21 @@ bank_stocks <- cbind(BAC, C, GS, JPM, MS, WFC)
 # Set the column names
 colnames(bank_stocks) <- paste(rep(tickers, each = ncol(BAC)), colnames(BAC), sep = "_")
 
-# Create an empty data frame
-returns <- data.frame()
+# Create the candlestick chart
+fig <- plot_ly(x = ~BAC$Date, type="candlestick",
+               open = ~BAC$Open, close = ~BAC$Close,
+               high = ~BAC$High, low = ~BAC$Low) %>%
+  layout(title = "Bank of America Stock Price",
+         yaxis = list(title = "BAC Stock"),
+         shapes = list(
+           list(type = "line", x0 = '2009-01-20', x1 = '2009-01-20', y0 = 0, y1 = 1, yref = "paper", line = list(color = "black")),
+           list(type = "line", x0 = '2007-12-01', x1 = '2007-12-01', y0 = 0, y1 = 1, yref = "paper", line = list(color = "black"))
+         ),
+         annotations = list(
+           list(x = '2009-01-25', y = 0.9, text = 'President Obama Took Office', showarrow = FALSE, xref='x', yref='paper', xanchor = 'left'),
+           list(x = '2007-11-25', y = 0.05, text = 'Subprime Mortgage Crisis', showarrow = FALSE, xref='x', yref='paper', xanchor = 'right')
+         ))
 
-# Calculate the percentage change for each ticker
-for(tick in tickers){
-  returns[paste(tick, "Return", sep = " ")] <- c(NA, diff(log(bank_stocks[paste(tick, "Close", sep = "_")])))
-}
+# Display the plot
+fig
 
-# Remove the first row since it contains NA values
-returns <- returns[-1, ]
-
-# Create a pair plot
-ggpairs(returns)
