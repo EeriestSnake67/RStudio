d7fed17471fd66a125045275093de65d4ec4230d
Grace Leavitt <gleav8@gmail.com>
2024-01-18 13:42:42 -0500

    Made the 30 day moving average graph and created the slideshow To do tonight: Finish the standard deviations, create all of the moving averages (2008, in the middle, 2020), create the EDA in vs code, put them on the slideshow and start tying up loose ends

--- Histograms.R
+++ Histograms.R
@@ -24,6 +24,9 @@ bank_stocks <- cbind(BAC, C, GS, JPM, MS, WFC)
 colnames(bank_stocks) <- paste(rep(tickers, each = ncol(BAC)), colnames(BAC), sep = "_")
 bank_stocks
 
+BAC3 <- read_csv("BAC.csv")
+BAC3
+
 std_df <- data.frame(ticker = tickers, std = sapply(tickers, function(tick) sd(bank_stocks[[paste(tick, 'Close', sep = '_')]], na.rm = TRUE)))
 
 # Create the graph for general standard deviation
@@ -53,24 +56,21 @@ ggplot(std_df, aes(x = ticker, y = std)) +
 
 #data set goes from 2006-2020 (looked at the riskiest stock in 2006 compared to 2020)
 
+BAC
+#moving average
+install.packages("zoo")
+library(zoo)
+BAC3$Date <- as.Date(BAC3$Date)
 
-#Histogram
-library(lubridate)
+# Filter for the year 2008
+BAC3_2008 <- BAC3 %>% filter(Date >= as.Date('2008-01-01') & Date <= as.Date('2008-12-31'))
 
-# Convert row names to Date
-rownames(returns) <- ymd(rownames(returns))
+# Calculate 30 Day Moving Average
+BAC3_2008$MA30 <- rollmean(BAC3_2008$Close, 30, fill = NA)
 
-# Create a list to store the plots
-plots <- list()
-
-# Create a histogram for each bank
-for (tick in tickers) {
-  p <- ggplot(data = subset(returns, between(as.Date(rownames(returns)), as.Date('2018-01-01'), as.Date('2019-12-31'))), aes_string(x = paste(tick, 'Return', sep = ' '))) +
-    geom_histogram(bins = 50, fill = "blue") +  labs(x = "Return", y = "Frequency", title = paste(tick, "Returns (2018 - 2019)")) +
-    theme_minimal() +
-    theme(plot.title = element_text(size = 15))
-  plots[[tick]] <- p
-}
-
-# Combine the plots into a grid
-grid.arrange(grobs = plots, ncol = 2)
+# Plot
+ggplot(BAC3_2008, aes(x = Date)) +
+  geom_line(aes(y = Close, color = 'BAC Close Price')) +
+  geom_line(aes(y = MA30, color = '30 Day Moving Average')) +
+  labs(title = 'Bank Of America Moving Average', y = 'Close Price', x = '', color = '') +
+  theme_minimal()
